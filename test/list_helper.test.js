76795bf6e68ccd61732e0b436cfce52c26b58ab4
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2024-11-30 19:32:41 +0500

    completed exercises 4.6 to 4.7

--- test/list_helper.test.js
+++ test/list_helper.test.js
@@ -147,3 +147,179 @@ describe('favorite blog', () => {
     assert.deepStrictEqual(result, listWithMultipleBlog[1]);
   });
 });
+
+describe('author with most blogs', () => {
+  test('of single blog', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+        __v: 0
+      }
+    ];
+    const result = listHelper.mostBlogs(listWithOneBlog);
+    assert.deepStrictEqual(result, { author: 'Edsger W. Dijkstra', blogs: 1 });
+  });
+  test('of multiple blogs', () => {
+    const listWithMultipleBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 100,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 15,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+        __v: 0
+      }
+    ];
+    const result = listHelper.mostBlogs(listWithMultipleBlog);
+    assert.deepStrictEqual(result, { author: 'Edsger W. Dijkstra', blogs: 3 });
+  });
+  test('of multiple blogs with more than 1 items with same likes', () => {
+    const listWithMultipleBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 100,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 150,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 150,
+        __v: 0
+      }
+    ];
+    const result = listHelper.mostBlogs(listWithMultipleBlog);
+    assert.deepStrictEqual(result, { author: 'Edsger W. Dijkstra', blogs: 2 });
+  });
+});
+
+describe('author with most likes', () => {
+  test('of single blog', () => {
+    const listWithOneBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+        __v: 0
+      }
+    ];
+    const result = listHelper.mostLikes(listWithOneBlog);
+    assert.deepStrictEqual(result, { author: 'Edsger W. Dijkstra', likes: 10 });
+  });
+  test('of multiple blogs', () => {
+    const listWithMultipleBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 100,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 15,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 10,
+        __v: 0
+      }
+    ];
+    const result = listHelper.mostLikes(listWithMultipleBlog);
+    assert.deepStrictEqual(result, {
+      author: 'Edsger W. Dijkstra',
+      likes: 125
+    });
+  });
+  test('of multiple blogs with more than 1 items with same likes', () => {
+    const listWithMultipleBlog = [
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 100,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 150,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 150,
+        __v: 0
+      }
+    ];
+    const result = listHelper.mostLikes(listWithMultipleBlog);
+    assert.deepStrictEqual(result, {
+      author: 'Edsger W. Dijkstra',
+      likes: 300
+    });
+  });
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -23,8 +23,65 @@ const favoriteBlog = (blogs) => {
   return higestRatedBlog;
 };
 
+function mostBlogs(blogs) {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authorBlogCounts = {};
+
+  blogs.forEach((blog) => {
+    authorBlogCounts[blog.author] = (authorBlogCounts[blog.author] || 0) + 1;
+  });
+
+  let maxAuthor = null;
+  let maxBlogs = 0;
+
+  for (const [author, count] of Object.entries(authorBlogCounts)) {
+    if (count > maxBlogs) {
+      maxAuthor = author;
+      maxBlogs = count;
+    }
+  }
+
+  return {
+    author: maxAuthor,
+    blogs: maxBlogs
+  };
+}
+
+function mostLikes(blogs) {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authorLikesCounts = {};
+
+  blogs.forEach((blog) => {
+    authorLikesCounts[blog.author] =
+      (authorLikesCounts[blog.author] || 0) + blog.likes;
+  });
+
+  let maxAuthor = null;
+  let maxLikes = 0;
+
+  for (const [author, count] of Object.entries(authorLikesCounts)) {
+    if (count > maxLikes) {
+      maxAuthor = author;
+      maxLikes = count;
+    }
+  }
+
+  return {
+    author: maxAuthor,
+    likes: maxLikes
+  };
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
